180d060efe613f19842967980ab8e542deed3e1b
5veta <1985vet@gmail.com>
2020-07-01 01:48:21 +0300

    continue

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -6,8 +6,8 @@ import {Link} from 'react-router-dom';
 const Footer=({lang, isLogin})=>{
     
     return (
-        <div class="d-flex justify-content-md-center bgstyle py-2">
-            <div className="bgstyle w-75 d-flex flex-row justify-content-start justify-content-md-between ">
+        <div class="d-flex justify-content-md-center bgstyle">
+            <div className="bgstyle w-75 py-2 d-flex flex-row justify-content-start align-middle justify-content-md-between ">
                 
                 <div>
                 {   
