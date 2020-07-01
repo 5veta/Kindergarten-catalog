a96b8ccb1d3b1981ecfea4012c60f46a4c55bddb
5veta <1985vet@gmail.com>
2020-07-01 22:29:27 +0300

    continue

--- src/components/ui/Menu.js
+++ src/components/ui/Menu.js
@@ -6,7 +6,7 @@ const Menu=({isLogin, login, lang})=>{
     
   return (
     <div class="d-flex justify-content-md-center">
-      <div class="w-75 d-flex justify-content-md-between">
+      <div class="w-75 d-flex flex-column flex-md-row justify-content-md-between">
         <nav className="navbar navbar-expand-md row">
           <a className="navbar-brand text-reset " href="/">
             <img class="d-none d-sm-none d-md-block childrenimg mr-5" src="/children.png" />
