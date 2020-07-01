2fd5f9be3034c1a8925c63dccf3fbcfe763f72fa
5veta <1985vet@gmail.com>
2020-07-01 20:52:24 +0300

    continue

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ const App =()=>
         <Switch>
                 <Route exact path="/moder" component={LoginAF}/>        
                 <Route path="/" component={()=>(
-                        <div className="mh-100">
+                        <div className="vh-100">
                         <MenuC />
                         <Switch>
                                 <Route exact path="/" component={FormtoSel}/>
