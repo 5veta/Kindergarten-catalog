d5bea629107048f0beedf0414892da682a82596c
5veta <1985vet@gmail.com>
2020-07-03 01:40:25 +0300

    continue

--- src/components/ui/Menu.js
+++ src/components/ui/Menu.js
@@ -23,6 +23,9 @@ const Menu=({isLogin, login, lang})=>{
               </li>
               <li className="nav-item ">
                 <ul className="navbar-nav navbar-center text-nowrap text-black  my-4 textnavy">
+                  <li className="d-block d-sm-block d-md-none nav-item ">
+                    <a href="/">{lang.home}</a>
+                  </li>
                   <li className="nav-item ">
                   {
                     (login==='admin')?
