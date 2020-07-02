22b3bd1f8270db86252e6c31d1d82037da24ab65
5veta <1985vet@gmail.com>
2020-07-03 00:22:06 +0300

    continue

--- src/components/ui/Menu.js
+++ src/components/ui/Menu.js
@@ -7,23 +7,15 @@ const Menu=({isLogin, login, lang})=>{
   return (
     <div class="d-flex justify-content-md-center">
       <div class="w-75 d-flex flex-row justify-content-between">
-        <nav className="navbar navbar-expand-md row">
-          <a className="navbar-brand text-reset " href="/">
-            <img class="d-none d-sm-none d-md-block childrenimg mr-5" src="/children.png" />
-            <img class="d-block d-sm-block d-md-none childrenimg-small mr-5" src="/children.png" />
-          </a>
-        </nav>
-        <nav className="navbar navbar-expand-md row">
-          <div className="col-1"><a className="navbar-brand text-reset " href="/">
-            <h3 class="m-0 p-0 d-none d-sm-none d-md-block textnavy">{lang.maineheader}</h3>
-          </a></div>
-        </nav>
-        <nav class="navbar navbar-expand-md row">
+        
+        <nav class="navbar navbar-expand-md column">
           <button class="navbar-toggler d-block d-sm-block d-md-none ml-5" data-toggle="collapse" data-target="#mainmenu" >
             &#9776;
           </button>
           <div id="mainmenu" class="collapse navbar-collapse">
-            <ul className="navbar-nav navbar-center text-nowrap text-black  my-4 textnavy">  
+            <ul className="navbar-nav navbar-center text-nowrap text-black  my-4 textnavy">
+              <li className="nav-item ">
+                <ul className="navbar-nav navbar-center text-nowrap text-black  my-4 textnavy">
               <li className="nav-item ">
               {
               (login==='admin')?
@@ -44,8 +36,19 @@ const Menu=({isLogin, login, lang})=>{
               <li className="nav-item mx-sm-0 mx-md-2 my-2 my-sm-2 my-md-0">
                 <LangMenu />
               </li>
+              </ul>
+              </li>
+              <li className="nav-item "><a className="navbar-brand text-reset " href="/">
+            <h3 class="m-0 p-0 d-none d-sm-none d-md-block textnavy">{lang.maineheader}</h3>
+          </a></li>
             </ul>
           </div>
+        </nav>
+        <nav className="navbar navbar-expand-md row">
+          <a className="navbar-brand text-reset " href="/">
+            <img class="d-none d-sm-none d-md-block childrenimg mr-5" src="/children.png" />
+            <img class="d-block d-sm-block d-md-none childrenimg-small mr-5" src="/children.png" />
+          </a>
         </nav>
       </div>
     </div>
