ca11f3c90f4a1b2ead0b8ddbd9282273bc868b4b
5veta <1985vet@gmail.com>
2020-07-11 16:43:29 +0300

    fixed bug

--- src/components/ui/FormHeader.js
+++ src/components/ui/FormHeader.js
@@ -1,12 +1,14 @@
 import { Form } from "redux-form";
 
+import PropTypes from 'prop-types';
+
 const FormHeader=header=>{
 
     return <h5 className="textnavy">{header}</h5>;
 };
 
 FormHeader.propTypes={
-    header: PropTypes.string.isRequired
+    header: PropTypes.string.isRequired,
 };
 
 export default FormHeader;
