44fd170e9461c8cbd557c312cb44b43b8f89357e
5veta <1985vet@gmail.com>
2020-07-11 21:56:33 +0300

    continue

--- src/components/App.js
+++ src/components/App.js
@@ -10,22 +10,23 @@ const App =()=>
         <Switch>
                 <Route exact path="/moder" component={LoginAF}/>        
                 <Route path="/" component={()=>(
-                        
-                        <MenuC />
-                        <Switch>
-                                <Route exact path="/" component={HomePage}/>
-                                <Route exact path="/sel/:cids" component={HomePage}/>
-                                <Route exact path="/sel/:cids/:regids" component={HomePage}/>
-                                <Route exact path="/sel/:cids/:regids/:locids" component={HomePage}/>
-                                <Route exact path="/kgardens/:id" component={KgardenDetails} />
-                                <Route path="/login" component={LoginF}/>
-                                <Route path="/createaccount" component={CreateAccountF}/>
-                                <Route path="/forgotpass" component={ForgotPass}/>
-                                <Route path="/recoverpass/:token" component={PasswordRecovery}/>
-                                <Route path="/accaunt" component={UserAccaunt}/>
-                                <Route path="/admin" component={AdmAccaunt}/>
-                        </Switch>
-                        <FooterC />
+                        <div>
+                                <MenuC />
+                                <Switch>
+                                        <Route exact path="/" component={HomePage}/>
+                                        <Route exact path="/sel/:cids" component={HomePage}/>
+                                        <Route exact path="/sel/:cids/:regids" component={HomePage}/>
+                                        <Route exact path="/sel/:cids/:regids/:locids" component={HomePage}/>
+                                        <Route exact path="/kgardens/:id" component={KgardenDetails} />
+                                        <Route path="/login" component={LoginF}/>
+                                        <Route path="/createaccount" component={CreateAccountF}/>
+                                        <Route path="/forgotpass" component={ForgotPass}/>
+                                        <Route path="/recoverpass/:token" component={PasswordRecovery}/>
+                                        <Route path="/accaunt" component={UserAccaunt}/>
+                                        <Route path="/admin" component={AdmAccaunt}/>
+                                </Switch>
+                                <FooterC />
+                        </div>
                 )}/>
         </Switch>
 export default App;
