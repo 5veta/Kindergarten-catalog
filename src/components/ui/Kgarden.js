46e9a1c256c600494e329682b68758e374fc2e09
5veta <1985vet@gmail.com>
2020-07-11 21:36:06 +0300

    continue

--- src/components/ui/Kgarden.js
+++ src/components/ui/Kgarden.js
@@ -105,7 +105,7 @@ const Kgarden=({kgarden, lang})=>{
     
     return(
     <div className="row">
-        <div className=" col-8 card mb-3" style={{borderColor: "#3b5f82"}}>
+        <div className=" col-12 card mb-3" style={{borderColor: "#3b5f82"}}>
             <div className="card-header  bg-light border-light">
                 <div className="card-group m-0">
                     <KgContacts site={kgarden.site} phonenumber={kgarden.phonen} address={kgarden.address} lang={lang.address} />
