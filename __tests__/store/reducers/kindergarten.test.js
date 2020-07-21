70df303dcf2784cc3289c2eab1944ba0c179ca1d
5veta <1985vet@gmail.com>
2020-07-21 23:27:08 +0300

    changed

--- __tests__/store/reducers/kindergarten.test.js
+++ __tests__/store/reducers/kindergarten.test.js
@@ -7,34 +7,36 @@ describe("kindergarten reducer", ()=>{
         const action={
             type: C.ADD_KGARDEN, 
             kgarden:{
-                kgid: 0, img: 'som.jpg', 
+                kgid: 60,
+                img: "som.jpg", 
                 name: "Name", 
                 descr: "some description", 
                 site: "http://somesite.com", 
-                phonen: "+380991112225", 
+                phonen: ["+380991112225"], 
                 time:{from: "8:00", to: "19:00"}, 
-                age:{from: 2, to: 6}, 
-                price:{halfday: 250, day: 500, month: 15000, year: 200000, annual: 5000, currency: ["грн.", "₴"] }, 
-                address: {aid:1, sid:2, house: '5', appt: '4', floor: '1'}, 
+                age:{from: "2", to: "6"}, 
+                price:{halfday: "250", day: "500", month: "15000", year: "200000", annual: "5000", currency: ["грн.", "₴"] }, 
+                address: {aid:"1", sid:"2", house: "5", appt: "4", floor: "1"},  
                 lessons: [{ldesc: "Розвиток мови, читання, азбука, навчання грамоті (формування зв’язного мовлення, розвиток словника, правильної вимови, навичок читати, знання алфавіту)",
                 lname: "Розвиток мови"}]
             }
         };
         const results=kgarden(state, action);
         expect(results)
-            .toEqual({
+        .toEqual({
                 kgid: 60,
-                img: '60.jpg', 
+                img: "som.jpg", 
                 name: "Name", 
                 descr: "some description", 
                 site: "http://somesite.com", 
-                phonen: "+380991112225", 
+                phonen: ["+380991112225"], 
                 time:{from: "8:00", to: "19:00"}, 
-                age:{from: 2, to: 6}, 
-                price:{halfday: 250, day: 500, month: 15000, year: 200000, annual: 5000, currency: ["грн.", "₴"] }, 
-                address: {aid:1, sid:2, house: '5', appt: '4', floor: '1'}, 
+                age:{from: "2", to: "6"}, 
+                price:{halfday: "250", day: "500", month: "15000", year: "200000", annual: "5000", currency: ["грн.", "₴"] }, 
+                address: {aid:"1", sid:"2", house: "5", appt: "4", floor: "1"}, 
                 lessons: [{ldesc: "Розвиток мови, читання, азбука, навчання грамоті (формування зв’язного мовлення, розвиток словника, правильної вимови, навичок читати, знання алфавіту)",
-                    lname: "Розвиток мови"}]
+                lname: "Розвиток мови"}]
+                
             });
     });
 });
